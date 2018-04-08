409029507f39ef04c76268f83d6582b540ac5d6b
montygoldy <montygoldy2006@gmail.com>
2018-04-08 15:19:51 -0400

    Used fetch api to make calls to test db

--- js/app.js
+++ js/app.js
@@ -1,7 +1,17 @@
 //Onclick play video
-$(document).ready(function(){
- $('#video-button').on('click', () => {
-   $('.video-section').prepend('<iframe src="https://www.youtube.com/embed/Ldjmb15Jsx0?controls=0&showinfo=0&rel=0&autoplay=1&loop=1&playlist=Ldjmb15Jsx0" frameborder="0" allowfullscreen></iframe>');
-   $(".video-section__content").hide();
- })
+$(document).ready(function() {
+  $("#video-button").on("click", () => {
+    $(".video-section").prepend(
+      '<iframe src="https://www.youtube.com/embed/Ldjmb15Jsx0?controls=0&showinfo=0&rel=0&autoplay=1&loop=1&playlist=Ldjmb15Jsx0" frameborder="0" allowfullscreen></iframe>'
+    );
+    $(".video-section__content").hide();
+  });
+
+  //Get Info from db
+  fetch("../db/test.json")
+    .then(res => res.json())
+    .then(data => console.log(data))
+    .catch(err => console.log(err));
+
+  //Build Element
 });
